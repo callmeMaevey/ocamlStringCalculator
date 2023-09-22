922b27eb67eeb0b79c702558a2b98de77c59cd0a
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-09-21 20:08:11 -0400

    initial commit

---
+++ main.ml
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+(* Trying to make a calculator that *)
+
+let int_of_char = function
+  | '1' -> 1
+  | '2' -> 2
+  | '3' -> 3
+  | '4' -> 4
+  | '5' -> 5
+  | '6' -> 6
+  | '7' -> 7
+  | '8' -> 8
+  | '9' -> 9
+  |  _  -> 0
+ 
+let charListAdd a b =
+ match a,b with
+  | [],[] -> []
+  | aheah::atail, bhead::btail -> match ah,bh with
+    | 
+  | _ -> ['0']
+
+
+
+
